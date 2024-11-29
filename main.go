76e760d71395acbc54f52007cb07a55bd880dfc0
yanasirina <yanasirina0@yandex.ru>
2024-11-29 13:25:26 +0300

    ping-ручка

--- main.go
+++ main.go
@@ -6,10 +6,20 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type PingResponse struct {
+	DaysBefore2025 int
+}
+
+func GetDaysBefore2025() int {
+	return 5
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.GET("/ping", func(c echo.Context) error {
+		daysBefore2025 := GetDaysBefore2025()
+		response := PingResponse{DaysBefore2025: daysBefore2025}
+		return c.JSON(http.StatusOK, response)
 	})
 	e.Logger.Fatal(e.Start(":1323"))
 }
