bbea3778bc3a20f6171508a8c2be3ed870aca411
yanasirina <yanasirina0@yandex.ru>
2024-11-29 13:48:34 +0300

    вынесла хэндлер в отдельную функцию + переименовала переменные

--- main.go
+++ main.go
@@ -14,12 +14,14 @@ func GetDaysBefore2025() int {
 	return 5
 }
 
+func PingHandler(context echo.Context) error {
+	daysBefore2025 := GetDaysBefore2025()
+	response := PingResponse{DaysBefore2025: daysBefore2025}
+	return context.JSON(http.StatusOK, response)
+}
+
 func main() {
-	e := echo.New()
-	e.GET("/ping", func(c echo.Context) error {
-		daysBefore2025 := GetDaysBefore2025()
-		response := PingResponse{DaysBefore2025: daysBefore2025}
-		return c.JSON(http.StatusOK, response)
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+	server := echo.New()
+	server.GET("/ping", PingHandler)
+	server.Logger.Fatal(server.Start(":1323"))
 }
