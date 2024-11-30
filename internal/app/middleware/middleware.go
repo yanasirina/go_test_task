6d0e394a0c3b37d3aecff176acac35791e16df9b
yanasirina <yanasirina0@yandex.ru>
2024-11-30 14:05:11 +0300

    вынесла роль в константы

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -12,11 +12,13 @@ func New() *Middleware {
 	return &Middleware{}
 }
 
+const roleAdmin = "admin"
+
 func (middleware *Middleware) RedButtonMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(context echo.Context) error {
 		headers := context.Request().Header
 		userRole := headers.Get("User-Role")
-		if userRole == "admin" {
+		if userRole == roleAdmin {
 			log.Println("red button user detected")
 		}
 		return next(context)
