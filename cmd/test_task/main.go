f86e231391177955458f3327fa600695c79020b4
yanasirina <yanasirina0@yandex.ru>
2024-11-30 13:32:09 +0300

    DI для сервиса

--- cmd/test_task/main.go
+++ cmd/test_task/main.go
@@ -4,11 +4,15 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/yanasirina/umbrella_corp_test_task/internal/app/endpoint"
 	"github.com/yanasirina/umbrella_corp_test_task/internal/app/middleware"
+	service2 "github.com/yanasirina/umbrella_corp_test_task/internal/app/service"
 )
 
 func main() {
 	server := echo.New()
 	server.Use(middleware.New().RedButtonMiddleware)
-	server.GET("/ping", endpoint.New().PingEndpoint)
+
+	service := service2.New()
+	server.GET("/ping", endpoint.New(service).PingEndpoint)
+
 	server.Logger.Fatal(server.Start(":1323"))
 }
